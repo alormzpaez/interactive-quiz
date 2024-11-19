616cb8fed470442e3fdfa3dceab75df2daf5ca5d
Alonso Ramírez Paez <alonso.ramirez@pertendo.com>
2024-11-18 23:54:20 -0600

    Added all problems about U2 "Solucion de Ecuaciones Lineales"

--- src/data/UnitTwoProblemsData.ts
+++ src/data/UnitTwoProblemsData.ts
@@ -8,33 +8,33 @@ export const UnitTwoProblemsData: MethodProblemsData[] = [
         problems: [
             {
                 type: 1,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = 5.739130435 ; b = 8.652173913 ; c = -0.391304347", "a = 5.740130435 ; b = 8.662173913 ; c = -0.381304347", "a = 5.738130435 ; b = 8.642173913 ; c = -0.381304347", "a = -5.739130435 ; b = -8.642173913 ; c = 0.391304347"],
+                correct_answer: "a = 5.739130435 ; b = 8.652173913 ; c = -0.391304347",
+                time: "12:30"
             },
             {
                 type: 2,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = 5.729130435 ; b = 8.642173913 ; c = -0.381304327", "a = 5.741130435 ; b = 8.552173913 ; c = -0.381304347", "a = 5.739130435 ; b = 8.652173913 ; c = -0.391304347", "No tiene respuesta este sistema de ecuaciones."],
+                correct_answer: "No tiene respuesta este sistema de ecuaciones.",
+                time: "12:30"
             },
             {
                 type: 3,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = -5.52173913 ; b = -8.695652174 ; c = -0.217391304", "a = 5.52173913 ; b = 8.695652174 ; c = -0.217391304", "a = 5.53173913 ; b = 8.705652174 ; c = -0.227391304", "a = 5.51273913 ; b = 8.685652114 ; c = 0.217391304"],
+                correct_answer: "a = 5.52173913 ; b = 8.695652174 ; c = -0.217391304",
+                time: "12:30"
             },
             {
                 type: 4,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = -2 ; b = 1 ; c = 2", "a = -1 ; b = 2 ; c = 1", "a = -1 ; b = -1 ; c = 2", "a = -2 ; b = -1 ; c = 2"],
+                correct_answer: "a = -2 ; b = 1 ; c = 2",
+                time: "12:30"
             },
             {
                 type: 5,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = 9 ; b = -3.5 ; c = 5.5", "a = 10 ; b = -3 ; c = 5", "No tiene respuesta este sistema de ecuaciones.", "a = 10 ; b = -9 ; c = 1"],
+                correct_answer: "a = 10 ; b = -3 ; c = 5",
+                time: "12:30"
             },
         ]
     },
@@ -44,33 +44,33 @@ export const UnitTwoProblemsData: MethodProblemsData[] = [
         problems: [
             {
                 type: 1,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = 5.739130435 ; b = 8.652173913 ; c = -0.391304347", "a = 5.740130435 ; b = 8.662173913 ; c = -0.381304347", "a = 5.738130435 ; b = 8.642173913 ; c = -0.381304347", "a = -5.739130435 ; b = -8.642173913 ; c = 0.391304347"],
+                correct_answer: "a = 5.739130435 ; b = 8.652173913 ; c = -0.391304347",
+                time: "12:30"
             },
             {
                 type: 2,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = 5.729130435 ; b = 8.642173913 ; c = -0.381304327", "a = 5.741130435 ; b = 8.552173913 ; c = -0.381304347", "a = 5.739130435 ; b = 8.652173913 ; c = -0.391304347", "No tiene respuesta este sistema de ecuaciones."],
+                correct_answer: "No tiene respuesta este sistema de ecuaciones.",
+                time: "12:30"
             },
             {
                 type: 3,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = -5.52173913 ; b = -8.695652174 ; c = -0.217391304", "a = 5.52173913 ; b = 8.695652174 ; c = -0.217391304", "a = 5.53173913 ; b = 8.705652174 ; c = -0.227391304", "a = 5.51273913 ; b = 8.685652114 ; c = 0.217391304"],
+                correct_answer: "a = 5.52173913 ; b = 8.695652174 ; c = -0.217391304",
+                time: "12:30"
             },
             {
                 type: 4,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = -2 ; b = 1 ; c = 2", "a = -1 ; b = 2 ; c = 1", "a = -1 ; b = -1 ; c = 2", "a = -2 ; b = -1 ; c = 2"],
+                correct_answer: "a = -2 ; b = 1 ; c = 2",
+                time: "12:30"
             },
             {
                 type: 5,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = 9 ; b = -3.5 ; c = 5.5", "a = 10 ; b = -3 ; c = 5", "No tiene respuesta este sistema de ecuaciones.", "a = 10 ; b = -9 ; c = 1"],
+                correct_answer: "a = 10 ; b = -3 ; c = 5",
+                time: "12:30"
             },
         ]
     },
@@ -80,33 +80,33 @@ export const UnitTwoProblemsData: MethodProblemsData[] = [
         problems: [
             {
                 type: 1,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = 5.739130435 ; b = 8.652173913 ; c = -0.391304347", "a = 5.740130435 ; b = 8.662173913 ; c = -0.381304347", "a = 5.738130435 ; b = 8.642173913 ; c = -0.381304347", "a = -5.739130435 ; b = -8.642173913 ; c = 0.391304347"],
+                correct_answer: "a = 5.739130435 ; b = 8.652173913 ; c = -0.391304347",
+                time: "12:30"
             },
             {
                 type: 2,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = 5.729130435 ; b = 8.642173913 ; c = -0.381304327", "a = 5.741130435 ; b = 8.552173913 ; c = -0.381304347", "a = 5.739130435 ; b = 8.652173913 ; c = -0.391304347", "No tiene respuesta este sistema de ecuaciones."],
+                correct_answer: "No tiene respuesta este sistema de ecuaciones.",
+                time: "12:30"
             },
             {
                 type: 3,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = -5.52173913 ; b = -8.695652174 ; c = -0.217391304", "a = 5.52173913 ; b = 8.695652174 ; c = -0.217391304", "a = 5.53173913 ; b = 8.705652174 ; c = -0.227391304", "a = 5.51273913 ; b = 8.685652114 ; c = 0.217391304"],
+                correct_answer: "a = 5.52173913 ; b = 8.695652174 ; c = -0.217391304",
+                time: "12:30"
             },
             {
                 type: 4,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = -2 ; b = 1 ; c = 2", "a = -1 ; b = 2 ; c = 1", "a = -1 ; b = -1 ; c = 2", "a = -2 ; b = -1 ; c = 2"],
+                correct_answer: "a = -2 ; b = 1 ; c = 2",
+                time: "12:30"
             },
             {
                 type: 5,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = 9 ; b = -3.5 ; c = 5.5", "a = 10 ; b = -3 ; c = 5", "No tiene respuesta este sistema de ecuaciones.", "a = 10 ; b = -9 ; c = 1"],
+                correct_answer: "a = 10 ; b = -3 ; c = 5",
+                time: "12:30"
             },
         ]
     },
@@ -116,33 +116,33 @@ export const UnitTwoProblemsData: MethodProblemsData[] = [
         problems: [
             {
                 type: 1,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = 0.13173992 ; b = 1.39828388 ; c = -2.13152324", "a = 0.33173992 ; b = 1.39828388 ; c = -2.11132324", "a = 0.23173992 ; b = 1.38828388 ; c = -2.12132324", "No puede resolverse con este método debido a que los coeficientes de la diagonal principal no cumplen con las reglas del método."],
+                correct_answer: "No puede resolverse con este método debido a que los coeficientes de la diagonal principal no cumplen con las reglas del método.",
+                time: "25:00"
             },
             {
                 type: 2,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = -6.13171344 ; b = 8.1842281 ; c = 3.1121392", "a = 6.13171344 ; b = -8.1842281 ; c = 3.1121392", "No puede resolverse con este método debido a que los coeficientes de la diagonal principal no cumplen con las reglas del método.", "a = 6.13171344 ; b = 8.1842281 ; c = 3.1121392"],
+                correct_answer: "No puede resolverse con este método debido a que los coeficientes de la diagonal principal no cumplen con las reglas del método.",
+                time: "25:00"
             },
             {
                 type: 3,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = 0.98631823 ; b = 2.89201029 ; c = 6.87861912", "a = 0.12631823 ; b = -2.99201029 ; c = 6.98069912", "a = 0.99631823 ; b = 2.99201029 ; c = 6.97869912", "a = -0.98631823 ; b = 2.98201129 ; c = 7.10869911"],
+                correct_answer: "a = 0.99631823 ; b = 2.99201029 ; c = 6.97869912",
+                time: "25:00"
             },
             {
                 type: 4,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = -3.000025844 ; b = 2.500002424 ; c = 6.99988592", "a = 3.000015844 ; b = -2.500001424 ; c = 6.999985592", "a = 2.000011844 ; b = 2.499991424 ; c = 6.999974592", "a = 3.000011844 ; b = 2.500002424 ; c = -6.999585592"],
+                correct_answer: "a = 3.000015844 ; b = -2.500001424 ; c = 6.999985592",
+                time: "25:00"
             },
             {
                 type: 5,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = -0.99183133 ; b = 7.00771293 ; c = 8.01423881", "a = 0.98983133 ; b = 7.1011293 ; c = 8.01423881", "a = 0.98983133 ; b = 7.00881293 ; c = 8.02423881", "a = -0.98983133 ; b = 7.00771293 ; c = 8.01423881"],
+                correct_answer: "a = -0.98983133 ; b = 7.00771293 ; c = 8.01423881",
+                time: "25:00"
             },
         ]
     },
@@ -152,33 +152,33 @@ export const UnitTwoProblemsData: MethodProblemsData[] = [
         problems: [
             {
                 type: 1,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = 0.13173992 ; b = 1.39828388 ; c = -2.13152324", "a = 0.33173992 ; b = 1.39828388 ; c = -2.11132324", "a = 0.23173992 ; b = 1.38828388 ; c = -2.12132324", "No puede resolverse con este método debido a que los coeficientes de la diagonal principal no cumplen con las reglas del método."],
+                correct_answer: "No puede resolverse con este método debido a que los coeficientes de la diagonal principal no cumplen con las reglas del método.",
+                time: "25:00"
             },
             {
                 type: 2,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = -6.13171344 ; b = 8.1842281 ; c = 3.1121392", "a = 6.13171344 ; b = -8.1842281 ; c = 3.1121392", "No puede resolverse con este método debido a que los coeficientes de la diagonal principal no cumplen con las reglas del método.", "a = 6.13171344 ; b = 8.1842281 ; c = 3.1121392"],
+                correct_answer: "No puede resolverse con este método debido a que los coeficientes de la diagonal principal no cumplen con las reglas del método.",
+                time: "25:00"
             },
             {
                 type: 3,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = 0.99311821 ; b = 2.92201021 ; c = 6.99869912", "a = 0.12631823 ; b = -2.99201029 ; c = 6.98069912", "a = 0.99631823 ; b = 2.99201029 ; c = 6.97869912", "a = -0.98631823 ; b = 2.98201129 ; c = 7.10869911"],
+                correct_answer: "a = 0.99311821 ; b = 2.92201021 ; c = 6.99869912",
+                time: "25:00"
             },
             {
                 type: 4,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = 3.000000352 ; b = -2.500000036 ; c = 6.999999989", "a = 3.000015844 ; b = -2.500001424 ; c = 6.999985592", "a = -2.000011844 ; b = 2.499991424 ; c = 6.999974592", "a = 3.000011844 ; b = -2.500002424 ; c = 6.999585592"],
+                correct_answer: "a = 3.000000352 ; b = -2.500000036 ; c = 6.999999989",
+                time: "25:00"
             },
             {
                 type: 5,
-                options: ["4", "5", "6", "7"],
-                correct_answer: "4",
-                time: "5:00"
+                options: ["a = -0.99183133 ; b = 7.00771293 ; c = 8.01423881", "a = 0.98983133 ; b = 7.1011293 ; c = 8.01423881", "a = 0.98983133 ; b = 7.00881293 ; c = 8.02423881", "a = -0.98883134 ; b = 7.00651297 ; c = 8.01523823"],
+                correct_answer: "a = -0.98883134 ; b = 7.00651297 ; c = 8.01523823",
+                time: "25:00"
             },
         ]
     },
